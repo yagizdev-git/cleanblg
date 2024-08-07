e7077b05649d64294dc7c7b2ee1d19cc2606c768
Yagiz <146383293+yagizdev-git@users.noreply.github.com>
2024-08-07 23:16:21 +0300

    dynamic page, routes and partials created

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const ejs = require('ejs');
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Statics
+app.use(express.static('public'));
+
+// Routes
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
